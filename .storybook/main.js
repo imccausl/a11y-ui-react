32aa52cd7b50eb6a4b00fd74a433fa591368a4bc
Ian McCausland <imccausl@gmail.com>
2020-10-04 02:35:55 -0400

    feat: add initial structure for UI library

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  stories: [
+    '../packages/**/*.stories.mdx',
+    '../packages/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+  ],
+}
