827b572c2a054b41777e87ed3500dc3608168ade
Ian McCausland <imccausl@gmail.com>
2020-10-04 18:59:53 -0400

    feat: get build process working

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,30 @@
-module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: [['module-resolver', { root: ['./packages'] }]],
+const BABEL_ENV = process.env.BABEL_ENV
+const isCommonJS = BABEL_ENV !== undefined && BABEL_ENV === 'cjs'
+const isESM = BABEL_ENV !== undefined && BABEL_ENV === 'esm'
+
+module.exports = function (api) {
+  api.cache(true)
+
+  const presets = [
+    [
+      '@babel/preset-env',
+      {
+        modules: isCommonJS ? 'cjs' : false,
+        targets: {
+          esmodules: isESM ? true : undefined,
+        },
+      },
+    ],
+    '@babel/preset-react',
+  ]
+
+  const plugins = [['module-resolver', { root: ['./packages'] }]]
+
+  const ignore = ['packages/**/*.test.js', '**/__tests__']
+
+  return {
+    presets,
+    plugins,
+    ignore,
+  }
 }
