44edf80682755eb52d94ab197ac34c72e8a97679
Ian McCausland <imccausl@gmail.com>
2022-09-10 20:14:35 -0400

    chore: upgrade yarn to modern

--- packages/button/src/index.js
+++ packages/button/src/index.js
@@ -1,17 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const ALLOWED_TYPES = ['button', 'submit']
+import { BaseButtonStyled } from './Button.styles'
 
-const Button = ({ children, type, ...restProps }) => {
-  return <button type={type}>{children}</button>
+const ALLOWED_TYPES = ['button', 'submit']
+const ALLOWED_STYLES = ['raised', 'flat']
+const ALLOWED_THEMES = ['primary', 'secondary']
+
+const Button = ({ children, type, onClick, disabled, ...restProps }) => {
+  const handleOnClick = (e) => {
+    if (!disabled) {
+      onClick(e)
+    }
+  }
+
+  return (
+    <BaseButtonStyled
+      {...restProps}
+      isDisabled={disabled}
+      type={type}
+      onClick={handleOnClick}
+    >
+      {children}
+    </BaseButtonStyled>
+  )
 }
 
 Button.propTypes = {
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
   type: PropTypes.oneOf(ALLOWED_TYPES),
 }
 
 Button.defaultProps = {
+  disabled: false,
   type: 'button',
 }
 
