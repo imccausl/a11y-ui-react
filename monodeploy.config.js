dec18442f06cb83e894c0182775b5c505d53a31a
Ian McCausland <imccausl@gmail.com>
2022-10-28 08:12:14 -0400

    chore: add monodeploy

---
+++ monodeploy.config.js
@@ -0,0 +1,12 @@
+module.exports = { 
+  preset: 'monodeploy/recommended',
+  git: {
+    commitSha: 'HEAD',
+    remote: 'origin',
+    push: true,
+  },
+  conventionalChangelogConfig: '@tophat/conventional-changelog-config',
+  access: 'infer',
+  persistVersions: false,
+  changesetIgnorePatterns: ['**/*.test.js'],
+}
