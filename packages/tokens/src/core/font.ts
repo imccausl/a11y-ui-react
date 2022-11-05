d0d2f41311c7bf43caea51334a877a8b7c272322
Ian McCausland <imccausl@gmail.com>
2022-11-05 18:07:53 -0400

    feat: add design tokens package

---
+++ packages/tokens/src/core/font.ts
@@ -0,0 +1,12 @@
+export const font = {
+  family: "'Inter', sans-serif",
+  weight: {
+    regular: {value: "400"},
+    medium: {value: "500"},
+    bold: {value: "600"},
+  },
+  spacing: {
+    normal: {value: "normal"},
+    wide: {value: "0.5"}
+  }
+}
